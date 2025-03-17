89f145073922a91d19e6e5b6e9bc00d24fbdf772
Mayko Costa <maykopr@hotmail.com>
2025-03-17 15:23:20 -0300

    refactor(index.js): altera a função addTask e renomeia as funções para legibilidade

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
-let text = document.getElementById("caixaTexto");
-let button = document.getElementById("botao");
-let areaList = document.getElementById("areaLista");
-let contador = 0;
+const textInput = document.getElementById("input");
+const btnAdd = document.getElementById("btn-add");
+const listArea = document.getElementById("list-area");
+btnAdd.onclick = addTask;
+let counter = 0;
 
-function addTarefa() {
-      let valorInput = text.value;
-      if (valorInput !== null && valorInput !== "" && valorInput !== undefined) {
-            ++contador;
+/**
+ * Add a new task to the list
+ *
+ * Adds a new task based on the user's input and appends it to the list. If the input is empty, does nothing.
+ *
+ * @listens textInput#keyup
+ * @listens btnAdd#click
+ */
+function addTask() {
+      const inputValue = textInput.value.trim();
 
-            let novaTarefa = `
-        <div class="lista" id="${contador}" >
-        <label for="check${contador}" id="tarefa" ><input onclick="marcarTarefa(${contador})" type="checkbox" class="check" id="check${contador}">${valorInput}</label>
-        <button onclick="deletar(${contador})" type="submit" id="deleteButton"><img class="lixeira"src="img/delete.png" alt="icone lixeira"></button>
-        </div>`;
+      if (inputValue) {
+            const taskCount = ++counter;
 
-            /*add novo bloco html à lista e limpa a caixa de texto*/
-            areaList.innerHTML += novaTarefa;
-            text.value = "";
-            text.focus();
+            const newTaskHtml = `
+                  <div class="anotation-card" id="${taskCount}">
+                        <input onclick="markTask(${taskCount})" type="checkbox" class="check-box" id="check${taskCount}">
+                        <label for="check${taskCount}"></label>
+                        <p class="anotation-card__text">${inputValue}</p>
+                        <button onclick="removeTask(${taskCount})" type="button" class="anotation-card__button" id="deleteButton"><i class="fi fi-ss-trash-restore-alt"></i></button>
+                  </div>`;
+
+            listArea.insertAdjacentHTML("beforeend", newTaskHtml);
+            textInput.value = "";
+            textInput.focus();
       }
 }
 
-text.addEventListener("keyup", function (event) {
-      if (event.keyCode === 13) {
-            event.preventDefault();
-            button.click();
-      }
-});
 
-function deletar(id) {
-      let tarefaRem = document.getElementById(id);
-      tarefaRem.remove(id);
+
+function removeTask(id) {
+      const task = document.getElementById(id);
+      task.remove(id);
 }
 
-function marcarTarefa(id) {
-      let item = document.getElementById(id);
+function markTask(id) {
+      const item = document.getElementById(id);
       let classe = item.getAttribute("class");
       let check = document.getElementById(`check${id}`);
-      console.log(id);
       if (classe == "lista") {
             item.classList.add("clicado");
       } else {
             item.classList.remove("clicado");
       }
 }
+
+textInput.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+            event.preventDefault();
+            addTask();
+      }
+});
